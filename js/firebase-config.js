809345bdd64162e8d820ec72ea53c7b1875868e6
Pak Guru AI <pakgurubajuri39@gmail.com>
2025-10-06 15:57:40 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,12 @@
-// Firebase configuration - GANTI DENGAN CONFIG ANDA SENDIRI
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-    authDomain: "your-project-id.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "123456789000",
-    appId: "1:123456789000:web:aaaaaaaaaaaaaaaaaaaaaa"
+  apiKey: "AIzaSyASpHaRhfJFUyhC3Osj24CTgU06Vsz10x0",
+  authDomain: "habits-agenda.firebaseapp.com",
+  projectId: "habits-agenda",
+  storageBucket: "habits-agenda.firebasestorage.app",
+  messagingSenderId: "1068837152911",
+  appId: "1:1068837152911:web:85111cd637f0840e4668fa",
+  measurementId: "G-LQZP1GL06X"
 };
 
 // Initialize Firebase
